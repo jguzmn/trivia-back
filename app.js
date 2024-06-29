24e8f36d3597d5a1a4b9f00018aed62ec40ee3df
jeguzman <guzmanmercado.je@gmail.com>
2024-06-28 21:18:43 -0500

    Second commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express'
-import mongoose from 'mongoose';
+import mongoose, { Schema,model } from 'mongoose';
+
 
 const app = express()
 const port = process.env.PORT;
@@ -15,11 +16,45 @@ console.log(url)
 
 app.use(express.json())
 
-try{
+try {
     await mongoose.connect(url)
     console.log('Database connected');
+} catch (error) {
+    console.log('Error database', error);
+}
+
+const user_schema = Schema({
+    name: {
+        type: String,
+        require: true
+    },
+    nickname: { 
+        type: String,
+        require: true
+    },
+    password: {type:String,require:true},
+    cel: {type:String},
+    email: {type:String},
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+const user = model('user',user_schema)
+
+const first_user = new user({
+    name: 'Jhon Doe',
+    nickname: 'jdoe',
+    password: '123',
+    cel: '',
+    email:'123@gmail.com'
+})
+
+try {
+    await first_user.save()
 } catch(error){
-    console.log('Error database',error);
+    console.log(error)
 }
 
 app.listen(port, () => console.log(`Server on port: ${port}`))
